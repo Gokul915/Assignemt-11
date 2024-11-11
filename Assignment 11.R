8aa6794419a0c5e738eaffde1be485a39c0f3dd2
Gokul915 <kolligokulkrishna@gmail.com>
2024-11-11 01:26:24 -0500

    Update Assignment 11.R final

--- Assignment 11.R
+++ Assignment 11.R
@@ -2,7 +2,7 @@ tukey_multiple <- function(x) {
   outliers <- array(TRUE,dim=dim(x))
   for (j in 1:ncol(x))
   {
-    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+    outliers[,j] <- outliers[,j] & tukey.outlier(x[,j])
   }
   outlier.vec <- vector(length=nrow(x))
   for (i in 1:nrow(x))
